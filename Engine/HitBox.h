c0a627d2e62c6ea904bd859dbc336e733d058693
mady981 <moritz-eibl@web.de>
2020-03-16 17:19:04 +0100

    HitBox all public

--- Engine/HitBox.h
+++ Engine/HitBox.h
@@ -5,7 +5,7 @@ public:
 	HitBox() = default;
 	HitBox( float x0_in,float y0_in,float x1_in,float y1_in );
 	bool operator==( const HitBox& rhs ) const;
-private:
+public:
 	float x0;
 	float y0;
 	float x1;
