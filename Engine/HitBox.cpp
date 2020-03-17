6dd786de93ec34fb1cf0657f19f73a1c234f1d43
mady981 <moritz-eibl@web.de>
2020-03-17 18:50:57 +0100

    HitBox operator== fixed

--- Engine/HitBox.cpp
+++ Engine/HitBox.cpp
@@ -1,15 +1,16 @@
 #include "HitBox.h"
 
-HitBox::HitBox( float x0_in,float y0_in,float x1_in,float y1_in )
+HitBox::HitBox( float left_in,float right_in,float top_in,float bottem_in )
 	:
-	x0( x0 ),
-	y0( y0 ),
-	x1( x1 ),
-	y1( y1 )
+	left( left_in ),
+	right( right_in ),
+	top( top_in ),
+	bottem( bottem_in )
 {
 }
 
-bool HitBox::operator==( const HitBox& rhs ) const
+bool HitBox::operator==( const HitBox& other ) const
 {
-	return x0 == rhs.x0 && y0 == rhs.y0 && x1 == rhs.x1 && y1 == rhs.y1;
+	return left > other.right && right < other.left
+		&& top > other.bottem && bottem < other.top;
 }
